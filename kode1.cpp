db87e0a9afdeb5e3d1266bb5d63dd44aaa7587ef
ANANDAGUSTI <anandagustisultan10@gmail.com>
2024-07-31 11:24:46 +0800

    selesai tugas

--- kode1.cpp
+++ kode1.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 using namespace std;
 
+void cetakdata(string nama, string nisn, string jurusan, int tinggibadan) {
+    cout << "Nama: " << nama << endl;
+    cout << "NISN: " << nisn << endl;
+    cout << "Jurusan: " << jurusan << endl;
+    cout << "Tinggi Badan: " << tinggibadan << endl;
+}
+
 int main() {
-    cout << "Selamat Datang di SMK TI Airlangga" << endl;
+    string nama = "Ananda Gusti Sultan";
+    string nisn = "0086892276";
+    string jurusan = "PPLG";
+    int tinggibadan = 170;
+    
+    cetakdata(nama, nisn, jurusan, tinggibadan);
+    
     return 0;
 }
